f57c20d52a86a517ed7ba18363e5fb64c641e3bd
Stefan Froemken <froemken@gmail.com>
2018-11-02 11:20:37 +0100

    Update to version 2.2.0 and set maps2 requirement to 4.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,12 @@ $EM_CONF['pfprojects'] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.1',
+    'version' => '2.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
-            'maps2' => '3.0.0-3.99.99',
-            'service_bw2' => '0.0.1-1.99.99'
+            'maps2' => '4.1.0-4.99.99',
+            'service_bw2' => '1.4.0-1.99.99'
         ],
         'conflicts' => [
         ],
