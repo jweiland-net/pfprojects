d6c4f1c9617c0fe6cd4a4f0676638743dca44e9a
Stefan Froemken <froemken@gmail.com>
2018-11-20 17:06:54 +0100

    Move all TCA modifying methods into Overrides

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,11 @@
 <?php
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JWeiland.pfprojects',
-    'Pfprojects',
-    'PF Projects'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'pfprojects',
-    'Configuration/TypoScript',
-    'PF Projects'
-);
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
     'tx_pfprojects_domain_model_project',
     'EXT:pfprojects/Resources/Private/Language/locallang_csh_tx_pfprojects_domain_model_project.xlf'
 );
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pfprojects_domain_model_project');
