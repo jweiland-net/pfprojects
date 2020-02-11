3e1aa6f805558a533fcf897600d9b80374f16975
Stefan Froemken <froemken@gmail.com>
2020-02-11 09:29:04 +0100

    Update to version 3.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF['pfprojects'] = [
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.0.1',
+    'version' => '3.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5..99',
-            'maps2' => '7.1.3-7.99.99',
+            'maps2' => '8.0.0-0.0.0',
             'service_bw2' => '3.0.0-3.99.99'
         ],
         'conflicts' => [
