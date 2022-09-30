d8be9c4409477ce649b9e1be5d80258fd4d29f2e
Stefan Froemken <froemken@gmail.com>
2022-09-30 11:18:15 +0200

    Add entry for new content element wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,31 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Pfprojects',
-    'Pfprojects',
-    [
-        \JWeiland\Pfprojects\Controller\ProjectController::class => 'list, search, show'
-    ],
-    // non-cacheable actions
-    [
-        \JWeiland\Pfprojects\Controller\ProjectController::class => 'search'
-    ]
-);
+call_user_func(static function (): void {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Pfprojects',
+        'Pfprojects',
+        [
+            \JWeiland\Pfprojects\Controller\ProjectController::class => 'list, search, show'
+        ],
+        // non-cacheable actions
+        [
+            \JWeiland\Pfprojects\Controller\ProjectController::class => 'search'
+        ]
+    );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['pfProjectUpdateSlug']
-    = \JWeiland\Pfprojects\Updates\SlugUpdateWizard::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['pfProjectUpdateSlug']
+        = \JWeiland\Pfprojects\Updates\SlugUpdateWizard::class;
+
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'ext-pfprojects-wizard-icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:pfprojects/Resources/Public/Icons/Extension.svg']
+    );
+
+    // Add pfprojects plugin to new element wizard
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pfprojects/Configuration/TSconfig/ContentElementWizard.tsconfig">'
+    );
+});
