85c055d1a4eaf31b88ed4b3714ac221a6899fb10
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-21 14:20:22 +0200

    [TASK] Updated version details

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,16 +4,16 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'PF Projects',
     'description' => 'This extension shows a list of projects and can be connected with service_bw2.',
     'category' => 'plugin',
-    'author' => 'Stefan Froemken',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '6.0.1',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.36-11.5.99',
-            'maps2' => '9.3.0-0.0.0',
-            'service_bw2' => '5.0.0-0.0.0',
+            'typo3' => '12.4.0-12.4.99',
+            'maps2' => '11.0.0-0.0.0',
+            'service_bw2' => '7.0.0-0.0.0',
         ],
         'conflicts' => [
         ],
