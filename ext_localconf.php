ee0285254b0e553cbf145a2e2e3561efeae7d101
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-21 15:36:00 +0200

    [TASK] Fixedgs

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,7 +18,4 @@ call_user_func(static function (): void {
             ProjectController::class => 'search',
         ]
     );
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['pfProjectUpdateSlug']
-        = SlugUpdateWizard::class;
 });
